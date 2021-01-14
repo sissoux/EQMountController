0befcfabf8e12ed20c33d895b85bf59c74682480
sissoux <alexis.damiens59@gmail.com>
2021-01-14 10:56:05 +0100

    Configured Hand controller for my hardware

--- addons/St4Serial/SmartHandController/Pinmap.h
+++ addons/St4Serial/SmartHandController/Pinmap.h
@@ -48,27 +48,20 @@
   #endif
 
   #ifdef __MYBOARD__
-    #define B_PIN0 21   // Shift
-    #define B_PIN1 15   // N
-    #define B_PIN2 16   // S
-    #define B_PIN3 17   // E
-    #define B_PIN4 20   // W
-    #define B_PIN5 22   // F
-    #define B_PIN6 23   // f
+    #define B_PIN0 3   // Shift
+    #define B_PIN1 4   // N
+    #define B_PIN2 2   // S
+    #define B_PIN3 1   // E
+    #define B_PIN4 5   // W
+    #define B_PIN5 6   // F
+    #define B_PIN6 0   // f
 
-    #define ST4RAw 2    // ST4 (OnStep) RA- West,  send data to OnStep
-    #define ST4DEs 3    // ST4 (OnStep) DE- South, clock input to ISR
-    #define ST4DEn 4    // ST4 (OnStep) DE+ North, recv data from OnStep
-    #define ST4RAe 5    // ST4 (OnStep) RA+ East,  always 25 Hz square wave on this pin
+    #define ST4RAw 12    // ST4 (OnStep) RA- West,  send data to OnStep
+    #define ST4DEs 13    // ST4 (OnStep) DE- South, clock input to ISR
+    #define ST4DEn 11    // ST4 (OnStep) DE+ North, recv data from OnStep
+    #define ST4RAe 10    // ST4 (OnStep) RA+ East,  always 25 Hz square wave on this pin
 
-    #if ST4_INTERFACE == ON
-      #define ST4AuxRAw 6 // ST4 (Aux) RA- West
-      #define ST4AuxDEs 7 // ST4 (Aux) DE- South
-      #define ST4AuxDEn 8 // ST4 (Aux) DE+ North
-      #define ST4AuxRAe 9 // ST4 (Aux) RA+ East
-    #endif
-
-    #define UTILITY_LIGHT_PIN  10    // Pin10 used as PWM output to drive utility lamp LED
+    #define UTILITY_LIGHT_PIN  9    // Pin10 used as PWM output to drive utility lamp LED
 
     #define DISPLAY_RESET_PIN  14    // Pin14 used to reset some displays, i.e. the SSD1309
   #endif
